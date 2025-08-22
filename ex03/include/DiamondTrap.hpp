57a94531db3e283b0ea56287b8d5fa420370e932
Jariskan <sergio.marin.alvarez@gmail.com>
2025-08-22 09:41:43 +0200

    Agregar la clase DiamondTrap con constructores, destructor y método whoAmI; actualizar archivos de encabezado y fuente.

--- ex03/include/DiamondTrap.hpp
+++ ex03/include/DiamondTrap.hpp
@@ -6,13 +6,34 @@
 /*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 20:12:58 by sergio            #+#    #+#             */
-/*   Updated: 2025/08/22 09:24:59 by sergio           ###   ########.fr       */
+/*   Updated: 2025/08/22 09:41:21 by sergio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DIAMONDTRAP_HPP
 # define DIAMONDTRAP_HPP
 
+#include "../include/ScavTrap.hpp"
+#include "../include/FragTrap.hpp"
 
+class DiamondTrap : public ScavTrap, public FragTrap
+{
+	private:
+		std::string _name;
+
+	public:
+		//OCF
+		DiamondTrap();										// Constructor x defecto
+		DiamondTrap(const DiamondTrap& other);				// Constructor x copia
+		DiamondTrap& operator=(const DiamondTrap& other);	// Operador de asignacion
+		~DiamondTrap();										// Destructor x defecto
+
+		// Constructor x parametros
+		DiamondTrap(const std::string& name);
+
+		// Metodos
+		void whoAmI();
+		using ScavTrap::attack;
+};
 
 #endif
